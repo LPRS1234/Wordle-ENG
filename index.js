476d7db01ffe26d9026b6eaa75fcb4eedad71b0e
LPRS1234 <lhsnkkh@gmail.com>
2025-10-19 01:25:22 +0900

    Result page, color, WarnMSG

--- index.js
+++ index.js
@@ -14,83 +14,166 @@ console.log(answer);
 let currentRow = 0;
 let isPlaying = true;
 
-function StartGame() {
-  // input 열 nodelist 로 가져오기
-  const inputRows = document.querySelectorAll(".wordle-row");
-  // NodeList to Array
-  let currentRowInputs = [...inputRows[currentRow].children];
 
-  if (currentRowInputs) {
-    currentRowInputs.forEach((input) => {
-      input.removeAttribute("disabled");
-    });
-    currentRowInputs[0].focus();
-  }
+// input 열 nodelist 로 가져오기
+const inputRows = document.querySelectorAll(".wordle-row");
+// NodeList to Array
+let currentRowInputs = [...inputRows[currentRow].children];
 
-  const keyboard = [
-    "a","b","c","d","e","f","g","h","i","j",
-    "k","l","m","n","o","p","q","r","s","t",
-    "u","v","w","x","y","z","backspace"
-  ];
-  
-  currentRowInputs.forEach((input, index) => {
-    input.addEventListener("keyup", (e) => {
-        const userKeyEvent = e.key.toLowerCase()
-        console.log(userKeyEvent)
-        if(keyboard.includes(`${userKeyEvent}`)) { //알파벳과 Backspace 선별
-            if(e.key !== "Backspace") {
-                if(index < 3) {
-                    //input에 입력시 다음칸으로 이동
-                    currentRowInputs[index+1].focus()
-                }
-            } else {
-                //백스페이스 눌렀을때 이전 칸 이동
-                if(!currentRowInputs[index].value) {
-                    if(index > 0) {
-                        currentRowInputs[index-1].focus()
-                    }
-                }
-            }
-        }
-    })
-  })
-
-  //채점 및 다음 줄 이동
-  document.addEventListener("keypress", (e) => {
-    //Enter키 입력확인
-      if(e.key === "Enter") {        
-        //글자 합치기
-        let userAnswer = ""
-        for(let i=0; i < 4; i++) {
-            userAnswer += currentRowInputs[i].value
-        }
-
-        //현재 열의 input 안에 모두 글자가 입력된 상태인지 확인
-        if(userAnswer.length == 4) {
-            if(userAnswer === answer) { //답 비교
-                //색 처리
-                document.write("Correct!!")
-            } else{
-                //색처리 후 다음줄로 이동
-                currentRowInputs.forEach((input) => {
-                    input.setAttribute("disabled", "");
-                });
-                currentRow++;
-                currentRowInputs = [...inputRows[currentRow].children];
-                currentRowInputs.forEach((input) => {
-                    input.removeAttribute("disabled");
-                });
-                currentRowInputs[0].focus();
-                // 네글자 넘어가면 실패화면 표시
-            }
-        } else {
-            // 네글자 모두 입력하라고 안내
-        }
-
-    }
-  })
-
+//현재 줄 비활성화 해제
+if (currentRowInputs) {
+  currentRowInputs.forEach((input) => {
+    input.removeAttribute("disabled");
+  });
+  currentRowInputs[0].focus();
 }
 
-StartGame();
-//disabled 클래스
+const keyboard = [..."abcdefghijklmnopqrstuvwxyz", "backspace"];
+
+const board = document.querySelector(".inputs");
+
+board.addEventListener("keyup", (e) => {
+  const input = e.target;
+
+  if (input.tagName !== "INPUT") return; //input이 아닌 태그에 대한 이벤트 다 제외
+
+  const userKeyEvent = e.key.toLowerCase();
+  if (!keyboard.includes(userKeyEvent)) return; //허용 키 선별
+
+  let index = currentRowInputs.indexOf(input);
+
+  if (e.key !== "Backspace") {
+    if (index < 3) {
+      //input에 입력시 다음칸으로 이동
+      currentRowInputs[index + 1].focus();
+    }
+  } else {
+    //백스페이스 눌렀을때 이전 칸 이동
+    if (!currentRowInputs[index].value) {
+      if (index > 0) {
+        currentRowInputs[index - 1].focus();
+      }
+    }
+  }
+});
+
+//결과 화면 가져오기
+const popUpPage = document.querySelector(".popup-page");
+
+//채점 및 다음 줄 이동
+document.addEventListener("keypress", (e) => {
+  //색 정의
+  const green = "#538D4E";
+  const yellow = "#B59F3B";
+  const grey = "#3A3A3C";
+  const text = "#F8F8F8";
+
+  // 정답 분리
+  let splitedAnswer = answer.split("");
+
+
+  //Enter키 입력확인
+  if (e.key === "Enter") {
+    //글자 합치기
+    let userAnswer = "";
+    for (let i = 0; i < 4; i++) {
+      userAnswer += currentRowInputs[i].value;
+    }
+
+    //현재 열의 input 안에 모두 글자가 입력된 상태인지 확인
+    if (userAnswer.length == 4) {
+      if (userAnswer === answer) {
+        //답 비교
+        currentRowInputs.forEach((input) => {
+          //색 처리
+          input.style.color = text;
+          input.style.backgroundColor = green;
+          //현재 줄 비활성화
+          input.setAttribute("disabled", "");
+
+          //승리화면 표시
+
+          popUpPage.innerHTML = `
+            <div>
+                <h2>You Win!!</h2>
+                <h3>Answer was ${answer}!!</h3>
+                <button class="restart">Restart</button>
+            </div>
+          `;
+
+          popUpPage.classList.remove("none");
+        });
+      } else {
+        if (currentRow == 4) {
+          // 기회 모두 소진 시 실패화면 표시
+
+          currentRowInputs.forEach((input, index) => {
+            // 색처리, 줄 비활성화
+            const value = input.value;
+            if (value === splitedAnswer[index]) {
+              input.style.color = text;
+              input.style.backgroundColor = green;
+              input.setAttribute("disabled", "");
+            } else if (splitedAnswer.includes(value)) {
+              input.style.color = text;
+              input.style.backgroundColor = yellow;
+              input.setAttribute("disabled", "");
+            } else {
+              input.style.color = text;
+              input.style.backgroundColor = grey;
+              input.setAttribute("disabled", "");
+            }
+          });
+
+          //실패 화면 띄우기
+          popUpPage.innerHTML = `
+            <div>
+                <h2>You Lose..</h2>
+                <h3>Answer is ${answer}!</h3>
+                <button class="restart">Restart</button>
+            </div>
+          `;
+
+          popUpPage.classList.remove("none");
+          return;
+        }
+        //색처리 후 다음줄로 이동 (색처리 아직 안됨)
+
+        currentRowInputs.forEach((input, index) => {
+          // 색처리, 줄 비활성화
+          const value = input.value;
+          if (value === splitedAnswer[index]) {
+            input.style.color = text;
+            input.style.backgroundColor = green;
+            input.setAttribute("disabled", "");
+          } else if (splitedAnswer.includes(value)) {
+            input.style.color = text;
+            input.style.backgroundColor = yellow;
+            input.setAttribute("disabled", "");
+          } else {
+            input.style.color = text;
+            input.style.backgroundColor = grey;
+            input.setAttribute("disabled", "");
+          }
+        });
+
+        currentRowInputs.forEach((input) => {
+          input.setAttribute("disabled", "");
+        });
+        currentRow++;
+        currentRowInputs = [...inputRows[currentRow].children];
+        currentRowInputs.forEach((input) => {
+          input.removeAttribute("disabled");
+        });
+        currentRowInputs[0].focus();
+      }
+    } else {
+      //경고 메시지 띄우기
+      const warningMessage = document.querySelector(".warning");
+      warningMessage.classList.remove("none");
+      setTimeout(() => {
+        warningMessage.classList.add("none");
+      }, 3000);
+    }
+  }
+});
